ebce7c93544e4e964eb8904387ba7fdf6c7b8f38
Venky <venkatramanm@gmail.com>
2019-05-14 22:04:53 +0530

    Inventory audits and adjustment api added

--- src/main/java/in/succinct/plugins/ecommerce/db/model/inventory/InventoryAudit.java
+++ src/main/java/in/succinct/plugins/ecommerce/db/model/inventory/InventoryAudit.java
@@ -1,8 +1,10 @@
 package in.succinct.plugins.ecommerce.db.model.inventory;
 
+import com.venky.swf.db.annotations.column.ui.HIDDEN;
 import com.venky.swf.db.model.Model;
 
 public interface InventoryAudit extends Model {
+    @HIDDEN
     public long getInventoryId();
     public void setInventoryId(long id);
     public Inventory getInventory();

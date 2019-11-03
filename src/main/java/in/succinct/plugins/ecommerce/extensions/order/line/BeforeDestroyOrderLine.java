c1309987165e36292cfd705b0d6d3242ad781fe3
Venky <venkatramanm@gmail.com>
2019-11-03 20:19:55 +0530

    Destroy Order Line to reduce demand

---
+++ src/main/java/in/succinct/plugins/ecommerce/extensions/order/line/BeforeDestroyOrderLine.java
@@ -0,0 +1,26 @@
+package in.succinct.plugins.ecommerce.extensions.order.line;
+
+import com.venky.swf.db.extensions.BeforeModelDestroyExtension;
+import com.venky.swf.plugins.background.core.Task;
+import com.venky.swf.plugins.background.core.TaskManager;
+import in.succinct.plugins.ecommerce.agents.demand.OpendDemandIncrementor;
+import in.succinct.plugins.ecommerce.agents.order.tasks.OrderStatusMonitor;
+import in.succinct.plugins.ecommerce.db.model.order.OrderLine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeforeDestroyOrderLine extends BeforeModelDestroyExtension<OrderLine> {
+    static {
+        registerExtension(new BeforeDestroyOrderLine());
+    }
+    @Override
+    public void beforeDestroy(OrderLine orderLine) {
+        List<Task> tasks = new ArrayList<>();
+        if (orderLine.getAcknowledgedQuantity() > 0 ){
+            tasks.add(new OpendDemandIncrementor(orderLine.getInventory(false).getId(),-orderLine.getAcknowledgedQuantity()));
+            tasks.add(new OrderStatusMonitor(orderLine.getOrderId()));
+        }
+        TaskManager.instance().executeAsync(tasks,false);
+    }
+}

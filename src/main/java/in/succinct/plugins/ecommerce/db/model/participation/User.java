cb42ada178c5582e500d6ab7672126ce31e26143
Venky <venkatramanm@gmail.com>
2020-05-08 14:14:24 +0530

    user delete to delete service resource

--- src/main/java/in/succinct/plugins/ecommerce/db/model/participation/User.java
+++ src/main/java/in/succinct/plugins/ecommerce/db/model/participation/User.java
@@ -1,12 +1,14 @@
 package in.succinct.plugins.ecommerce.db.model.participation;
 
 import com.venky.swf.db.annotations.column.relationship.CONNECTED_VIA;
+import com.venky.swf.db.annotations.column.ui.HIDDEN;
 import in.succinct.plugins.ecommerce.db.model.service.ServiceResource;
 
 import java.util.List;
 
 public interface User extends com.venky.swf.plugins.collab.db.model.user.User {
     @CONNECTED_VIA("USER_ID")
+    @HIDDEN
     public List<ServiceResource> getServiceResources();
 
 }

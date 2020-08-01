6968bb60ddbed2211704eeb4260a4a0d641ec903
Venky <venkatramanm@gmail.com>
2020-08-01 23:28:19 +0530

    Selling Price default to mrp

--- src/main/java/in/succinct/plugins/ecommerce/extensions/inventory/BeforeSaveInventory.java
+++ src/main/java/in/succinct/plugins/ecommerce/extensions/inventory/BeforeSaveInventory.java
@@ -1,8 +1,7 @@
 package in.succinct.plugins.ecommerce.extensions.inventory;
 
-import com.venky.core.math.DoubleUtils;
-import in.succinct.plugins.ecommerce.db.model.inventory.Inventory;
 import com.venky.swf.db.extensions.BeforeModelSaveExtension;
+import in.succinct.plugins.ecommerce.db.model.inventory.Inventory;
 import in.succinct.plugins.ecommerce.db.model.participation.MarketPlaceInventoryUpdateQueue;
 
 public class BeforeSaveInventory extends BeforeModelSaveExtension<Inventory> {
@@ -15,6 +14,10 @@ public class BeforeSaveInventory extends BeforeModelSaveExtension<Inventory> {
 		if (model.isInfinite() && model.getQuantity() > 0) {
 			model.setQuantity(0.0);
 		}
+		if (model.getSellingPrice() == null){
+			model.setSellingPrice(model.getSku().getMaxRetailPrice());
+		}
+
 		if (!model.isInfinite() ){
 			MarketPlaceInventoryUpdateQueue.push(model);
 		}

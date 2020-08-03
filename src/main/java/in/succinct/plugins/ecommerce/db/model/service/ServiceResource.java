d657b137518f33e5331bdec46048e31b80679b3e
Venky <venkatramanm@gmail.com>
2020-08-03 14:12:53 +0530

    Index Service Resource

--- src/main/java/in/succinct/plugins/ecommerce/db/model/service/ServiceResource.java
+++ src/main/java/in/succinct/plugins/ecommerce/db/model/service/ServiceResource.java
@@ -1,6 +1,8 @@
 package in.succinct.plugins.ecommerce.db.model.service;
 
 import com.venky.swf.db.annotations.column.IS_NULLABLE;
+import com.venky.swf.db.annotations.column.UNIQUE_KEY;
+import com.venky.swf.db.annotations.column.indexing.Index;
 import com.venky.swf.db.annotations.column.pm.PARTICIPANT;
 import com.venky.swf.db.annotations.model.EXPORTABLE;
 import com.venky.swf.db.model.Model;
@@ -10,13 +12,17 @@ import in.succinct.plugins.ecommerce.db.model.catalog.Service;
 
 public interface ServiceResource extends Model {
     @PARTICIPANT
+    @Index
+    @UNIQUE_KEY
     public Long getServiceId();
     public void setServiceId(Long id);
     public Service getService();
 
 
     @PARTICIPANT(redundant = true)
+    @Index
     @IS_NULLABLE(false)
+    @UNIQUE_KEY
     public Long getUserId();
     public void setUserId(Long userId);
     public User getUser();

f277aefd1c023845426ed7489cce33ec9eb79445
Venky <venkatramanm@gmail.com>
2020-11-10 18:14:04 +0530

    Reference un hidden

--- src/main/java/in/succinct/plugins/ecommerce/db/model/attachments/Attachment.java
+++ src/main/java/in/succinct/plugins/ecommerce/db/model/attachments/Attachment.java
@@ -1,21 +1,22 @@
 package in.succinct.plugins.ecommerce.db.model.attachments;
 
 import com.venky.swf.db.annotations.column.UNIQUE_KEY;
-import com.venky.swf.db.annotations.column.ui.HIDDEN;
+import com.venky.swf.db.annotations.column.ui.PROTECTION;
+import com.venky.swf.db.annotations.column.ui.PROTECTION.Kind;
 import in.succinct.plugins.ecommerce.db.model.inventory.Sku;
 import in.succinct.plugins.ecommerce.db.model.participation.Facility;
 
 public interface Attachment extends com.venky.swf.plugins.attachment.db.model.Attachment {
-    @HIDDEN
+    @PROTECTION(Kind.NON_EDITABLE)
     @UNIQUE_KEY
     public Long getFacilityId();
     public void setFacilityId(Long id);
     public Facility getFacility();
 
-    @HIDDEN
+    @PROTECTION(Kind.NON_EDITABLE)
     @UNIQUE_KEY
     public Long getSkuId();
     public void setSkuId(Long id);
-    public Sku getSku();
+    public Sku   getSku();
 
 }

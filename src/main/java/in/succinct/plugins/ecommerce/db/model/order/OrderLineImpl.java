d0f03acbf23764bd10ab3a6558afb39310cd76d6
Venky <venkatramanm@gmail.com>
2021-01-29 20:14:17 +0530

    Facility id column mistake

--- src/main/java/in/succinct/plugins/ecommerce/db/model/order/OrderLineImpl.java
+++ src/main/java/in/succinct/plugins/ecommerce/db/model/order/OrderLineImpl.java
@@ -279,7 +279,7 @@ public class OrderLineImpl  extends ModelImpl<OrderLine>{
                         ol.setShipFromId(shipNodeIds.get(0));
                     }else {
                         Select select = new Select().from(Facility.class);
-                        List<Facility> facilities = select.where(new Expression(select.getPool(),"FACILITY_ID" , Operator.IN, shipNodeIds.toArray())).execute();
+                        List<Facility> facilities = select.where(new Expression(select.getPool(),"ID" , Operator.IN, shipNodeIds.toArray())).execute();
                         Map<Long,Facility> facilityMap = new HashMap<>();
                         facilities.forEach(f->{
                             facilityMap.put(f.getId(),f);

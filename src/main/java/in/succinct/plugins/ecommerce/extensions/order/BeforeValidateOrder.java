609e8eff754eac31e9fa4eaf7a9c1f83df4061b6
Venky <venkatramanm@gmail.com>
2021-02-13 13:14:35 +0530

    Preferred Carrier not hard coded

---
+++ src/main/java/in/succinct/plugins/ecommerce/extensions/order/BeforeValidateOrder.java
@@ -0,0 +1,30 @@
+package in.succinct.plugins.ecommerce.extensions.order;
+
+import com.venky.swf.db.extensions.BeforeModelValidateExtension;
+import com.venky.swf.sql.Conjunction;
+import com.venky.swf.sql.Expression;
+import com.venky.swf.sql.Operator;
+import com.venky.swf.sql.Select;
+import in.succinct.plugins.ecommerce.db.model.order.Order;
+import in.succinct.plugins.ecommerce.db.model.participation.PreferredCarrier;
+
+import java.util.List;
+
+public class BeforeValidateOrder extends BeforeModelValidateExtension<Order> {
+    static {
+        registerExtension(new BeforeValidateOrder());
+    }
+    @Override
+    public void beforeValidate(Order model) {
+        if (model.getRawRecord().isFieldDirty("PREFERRED_CARRIER_NAME") && !model.getReflector().isVoid(model.getPreferredCarrierName())){
+            Expression where = new Expression(model.getReflector().getPool(), Conjunction.AND);
+            where.add(new Expression(model.getReflector().getPool(),"PREFERRED_CARRIER_NAME", Operator.EQ,model.getPreferredCarrierName()));
+            where.add(new Expression(model.getReflector().getPool(),"COMPANY_ID", Operator.EQ,model.getCompanyId()));
+
+            List<PreferredCarrier> carrierList = new Select().from(PreferredCarrier.class).where(where).execute(1);
+            if (carrierList.isEmpty()){
+                throw new RuntimeException(model.getCompany().getName()  + " does not have a relation ship with " + model.getPreferredCarrierName());
+            }
+        }
+    }
+}

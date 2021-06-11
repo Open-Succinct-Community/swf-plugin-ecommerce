dc15e787606f598d2e25b3272d571e5dd99c7f13
Prashant thakar <prashant@wiggles.in>
2021-06-11 18:48:46 +0530

    length expanded to 8192

--- src/main/java/in/succinct/plugins/ecommerce/db/model/inventory/Sku.java
+++ src/main/java/in/succinct/plugins/ecommerce/db/model/inventory/Sku.java
@@ -78,7 +78,7 @@ public interface Sku extends Model,Container, CompanySpecific {
 	public void setShortDescription(String shortDescription);
 
 	@Index
-	@COLUMN_SIZE(2048)
+	@COLUMN_SIZE(8192)
 	public String getLongDescription();
 	public void setLongDescription(String longDescription);
 

4d992a14f1df566f1a80dcb29c7d875b9be934a6
Venky <venkatramanm@gmail.com>
2024-03-02 16:20:49 +0530

    Lucene fixes

--- src/main/java/in/succinct/plugins/ecommerce/db/model/inventory/InventoryImpl.java
+++ src/main/java/in/succinct/plugins/ecommerce/db/model/inventory/InventoryImpl.java
@@ -3,16 +3,10 @@ package in.succinct.plugins.ecommerce.db.model.inventory;
 import com.venky.core.util.ObjectUtil;
 import com.venky.digest.Encryptor;
 import com.venky.swf.db.Database;
-import com.venky.swf.db.annotations.column.IS_VIRTUAL;
 import com.venky.swf.db.model.reflection.ModelReflector;
 import com.venky.swf.db.table.ModelImpl;
-import com.venky.swf.plugins.background.core.TaskManager;
 import com.venky.swf.plugins.calendar.db.model.WorkCalendar;
 import com.venky.swf.plugins.lucene.index.LuceneIndexer;
-import com.venky.swf.plugins.lucene.index.background.IndexManager;
-import com.venky.swf.plugins.lucene.index.background.IndexTask;
-import com.venky.swf.plugins.lucene.index.background.IndexTask.Operation;
-import com.venky.swf.routing.Config;
 import com.venky.swf.sql.Conjunction;
 import com.venky.swf.sql.Expression;
 import com.venky.swf.sql.Operator;
@@ -21,22 +15,16 @@ import in.succinct.plugins.ecommerce.db.model.assets.Asset;
 import in.succinct.plugins.ecommerce.db.model.assets.Capability;
 import in.succinct.plugins.ecommerce.db.model.attributes.AssetCodeAttribute;
 import in.succinct.plugins.ecommerce.db.model.catalog.Item;
-import in.succinct.plugins.ecommerce.db.model.inventory.InventoryCalculator.ATP;
 import in.succinct.plugins.ecommerce.db.model.participation.Company;
 import in.succinct.plugins.ecommerce.db.model.participation.Facility;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field.Store;
-import org.apache.lucene.document.TextField;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
-import java.util.logging.Level;
 
 public class InventoryImpl extends  ModelImpl<Inventory> {
 	public InventoryImpl(){
@@ -111,24 +99,11 @@ public class InventoryImpl extends  ModelImpl<Inventory> {
 
 	}
 	public void reindex(Inventory inventory){
-
-		LuceneIndexer indexer = LuceneIndexer.instance(Inventory.class);
-
 		try {
-			final Document document = indexer.getDocument(inventory.getRawRecord());
-			for (InventoryAttributeValue a : inventory.getInventoryAttributes()) {
-				indexer.addTextField(document, a.getAttribute().getName(), a.getAttributeValue().getPossibleValue(), Store.YES);
-			}
-			IndexTask task = new IndexTask();
-			task.setOperation(Operation.MODIFY);
-			task.setDirectory(Database.getTable(Inventory.class).getTableName());
-			task.setDocuments(Arrays.asList(document));
-			TaskManager.instance().executeAsync(task,false);
-
+			LuceneIndexer.instance(getModelClass()).updateDocument(inventory.getRawRecord());
 		}catch (Exception ex){
-			Config.instance().getLogger(getModelClass().getName()).log(Level.INFO,"Reindex on inventory failed" ,ex);
+			//
 		}
-
 	}
 
 	public List<Asset> getAssets() {
